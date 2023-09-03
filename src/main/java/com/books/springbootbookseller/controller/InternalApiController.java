d2458dff95c4053019d0767212d6144229bdaedb
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2023-09-03 12:50:43 +0200

    deploy heroku

--- src/main/java/com/books/springbootbookseller/controller/InternalApiController.java
+++ src/main/java/com/books/springbootbookseller/controller/InternalApiController.java
@@ -2,6 +2,7 @@ package com.books.springbootbookseller.controller;
 
 import com.books.springbootbookseller.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("api/internal")
 public class InternalApiController {
 
+  @Autowired
   private IUserService userService;
 
   @PutMapping("make-admin/{username}")
