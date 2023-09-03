fe817a4643caaf3a68428892787d731ecf44612d
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2023-09-03 13:01:16 +0200

    deploy heroku

--- front-angular/server.js
+++ front-angular/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const root = __dirname + "/dist/front-angular";
+const root = __dirname + "/dist/angular-book-seller";
 
 app.use(express.static(root));
+
 app.get('*', (req, res) => {
   res.sendFile('index.html', {root: root})
 });
