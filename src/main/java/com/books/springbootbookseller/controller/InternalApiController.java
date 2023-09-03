0d77de93f771afd64f2f9064132afbf7b80ad4ad
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2023-09-03 12:41:31 +0200

    deploy heroku

--- src/main/java/com/books/springbootbookseller/controller/InternalApiController.java
+++ src/main/java/com/books/springbootbookseller/controller/InternalApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/internal")
+@RequestMapping("api/internal")
 public class InternalApiController {
 
   private IUserService userService;
